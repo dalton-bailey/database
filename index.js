ac1c1cf1bc011d4b3806cee3f397387efe7151e7
Bailey Dalton <baidalton@gmail.com>
2020-10-19 23:02:24 -0600

    actual port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const port = proces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
